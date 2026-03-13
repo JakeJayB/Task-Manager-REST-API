b317509f850753247e6875d47b76f4b928b327ac
JakeJayB <JacobJBejarano@gmail.com>
2026-03-12 18:38:21 -0700

    Started on MongoDB integration

---
+++ MongoDB-Version/config.py
@@ -0,0 +1,12 @@
+# Script Purpose: Environment config for MongoDB. loads data from .env file
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    MONGODB_URI: str
+    DB_NAME: str
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- MongoDB-Version/database.py
+++ MongoDB-Version/database.py
@@ -1,5 +1,23 @@
-import models
+# Script Purpose: Client connection to the Task Manager database
 
-tasks : list[models.Task] = []
-next_id = 1
+from pymongo import AsyncMongoClient
+from config import settings
 
+async def main():
+    
+    client = AsyncMongoClient(settings.MONGODB_URI)
+
+    names = await client.list_database_names()
+
+    for name in names:
+        print(name)
+    db = client[settings.DB_NAME]
+
+
+    collection = db.get_collection('Tasks')
+
+    print(collection)
+
+
+import asyncio
+asyncio.run(main())
