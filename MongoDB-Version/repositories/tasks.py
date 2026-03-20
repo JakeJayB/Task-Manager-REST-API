ca155c3397a4fbe1559e51c295b2da11874db752
JakeJayB <JacobJBejarano@gmail.com>
2026-03-19 17:36:32 -0700

    Completed MongoDB-Version

--- MongoDB-Version/repositories/tasks.py
+++ MongoDB-Version/repositories/tasks.py
@@ -3,62 +3,52 @@ from bson import ObjectId
 import database as db
 
 
-def get_all(completed: bool | None = None, priority: Priority | None = None) -> list[Task]:
-    result = db.tasks
+async def get_all(
+    completed: bool | None = None, priority: Priority | None = None
+) -> list[Task]:
+    tasks = db.get_task_collection()
+    query = {}
 
     if completed is not None:
-        result = [task for task in result if task.completed == completed]
-    
+        query["completed"] = completed
+
     if priority is not None:
-        result = [task for task in result if task.priority == priority]
+        query["priority"] = priority
 
-    return result
-
-# def get_by_id(id: int) -> Task | None:
-#     for task in db.tasks:
-#         if task.id == id:
-#             return task
-#     return None
-
+    return await tasks.find(query).to_list()
 
 async def get_by_id(id: str) -> Task | None:
     collection = db.get_task_collection()
-    doc = await collection.find_one({"_id" : ObjectId(id)})
+    doc = await collection.find_one({"_id": ObjectId(id)})
     return Task(**doc) if doc else None
-
-# def create(task: CreateTask) -> Task:
-#     new_task = Task(id=db.next_id, **task.model_dump())
-#     db.tasks.append(new_task)
-#     db.next_id += 1
-#     return new_task
 
 async def create(task: CreateTask) -> Task:
     collection = db.get_task_collection()
     doc = await collection.insert_one(task.model_dump())
-    doc = await collection.find_one({'_id' : doc.inserted_id})
+    doc = await collection.find_one({"_id": doc.inserted_id})
     return Task(**doc)
-    
 
-def update(id: int, task: CreateTask) -> Task | None:
-    new_task = Task(id=id, **task.model_dump())
-    for i, existing_task in enumerate(db.tasks):
-        if existing_task.id == id:
-            db.tasks[i] = new_task
-            return new_task
-    return None
+async def update(id: str, task: CreateTask) -> Task | None:
+    from pymongo.collection import ReturnDocument
 
-def patch(id: int, task: UpdateTask) -> Task | None:
-    for i, existing_task in enumerate(db.tasks):
-        if existing_task.id == id:
-            updates = task.model_dump(exclude_unset=True)
-            updated_task = existing_task.model_copy(update=updates)
-            db.tasks[i] = updated_task
-            return updated_task
-    return None
+    tasks = db.get_task_collection()
+    doc = await tasks.find_one_and_replace(
+        {"_id": ObjectId(id)}, task.model_dump(), return_document=ReturnDocument.AFTER
+    )
+    return Task(**doc) if doc else None
 
-def delete(id: int) -> bool:
-    for i, existing_task in enumerate(db.tasks):
-        if existing_task.id == id:
-            db.tasks.pop(i)
-            return True
-    return False
+async def patch(id: str, task: UpdateTask) -> Task | None:
+    from pymongo.collection import ReturnDocument
+
+    tasks = db.get_task_collection()
+    doc = await tasks.find_one_and_update(
+        {"_id": ObjectId(id)},
+        {"$set": task.model_dump(exclude_unset=True)},
+        return_document=ReturnDocument.AFTER
+    )
+    return Task(**doc) if doc else None
+
+async def delete(id: str) -> bool:
+    tasks = db.get_task_collection()
+    response = await tasks.delete_one({"_id": ObjectId(id)})
+    return response.deleted_count == 1
--- MongoDB-Version/routers/tasks.py
+++ MongoDB-Version/routers/tasks.py
@@ -2,13 +2,12 @@ from fastapi import status, APIRouter
 from models import Task, CreateTask, UpdateTask, Priority
 from typing import Optional
 import services.tasks as task_service
-import asyncio
 
 router = APIRouter(prefix='/tasks', tags=['tasks'])
 
 @router.get('/', response_model=list[Task])
-def get_all_tasks(completed: Optional[bool] = None, priority: Optional[Priority] = None):
-    return task_service.get_all(completed, priority)
+async def get_all_tasks(completed: Optional[bool] = None, priority: Optional[Priority] = None):
+    return await task_service.get_all(completed, priority)
 
 @router.get('/{id}', response_model=Task)
 async def get_task(id: str):
@@ -19,13 +18,13 @@ async def create_task(task: CreateTask):
     return await task_service.create(task)
 
 @router.put('/{id}', response_model=Task)
-def update_task(id: int, task: CreateTask):
-    return task_service.update(id, task)
+async def update_task(id: str, task: CreateTask):
+    return await task_service.update(id, task)
 
 @router.patch('/{id}', response_model=Task)
-def patch_task(id: int, task: UpdateTask):
-    return task_service.patch(id, task)
+async def patch_task(id: str, task: UpdateTask):
+    return await task_service.patch(id, task)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_task(id: int):
-    task_service.delete(id)
+async def delete_task(id: str):
+    await task_service.delete(id)
